10811d866d75a35fa83bfd44f113b8f4eb063774
gustavorodrigues27 <goncalves.gustavo27@escola.pr.gov.br>
2024-09-05 10:55:16 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("   =================");
+    System.out.println("   = Curso técnico =");
+    System.out.println("   =================");
+    System.out.println("");
+    System.out.println(" > Desenvolvimento de sistemas  < ");
+    System.out.println("");
+    System.out.println("No início eu tinha eu fiquei bastante decepcionado pela matéria não ser muito o que eu esperava no começo, eu estava boiando no conteúdo.");
+    System.out.println("As tabelas verdades foram o que mais me complicaram porque eu não entendia nada das tabelas verdades, a forma com qual nós fazemos a prova me chamou bastante atenção por ser mais incomum de se fazer para mim é um ponto positivo.");
+    System.out.println("Eu estou achando o curso em si interessante nesse momento que estamos estudando a linguagem java, não sou muito chegado a java mas estou começando a entender o conteúdo.");
+    System.out.println(" já a minha opnião sobre o java, eu não sou muito familiarizado com essa linguagem porque eu programava na unity Engine, porém estou começando a gostar dessa lingugem de programação, penso que outras linguagem mais simples como o próprio pythone ou a godot que pode ser usada no mobile, seriam mais uma ótima possibilidade para o curso.");
+  }
+
+
+}
